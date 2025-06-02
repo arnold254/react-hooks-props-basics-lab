b2bc1078c0755f81b68fc702ff3ca5a507368ba5
Arnold <arnold.mutama@gmail.com>
2025-06-02 16:03:37 +0300

    Adds Components and props

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NavBar() {
+function Navbar() {
   return (
     <nav>
       <a href="#home">Home</a>
@@ -9,4 +9,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default Navbar;
